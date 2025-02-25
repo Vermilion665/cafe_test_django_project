5c7f404d97e4175484b2a3d31da180975155f6ac
Vermi <Vermilion666@list.ru>
2025-02-25 21:55:14 +0300

    renamed order to orders and create urls.py

--- base/settings.py
+++ base/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'order.apps.OrderConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
